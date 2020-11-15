cb0b25f0d5d03a6ad81ffe3fe8462831cf7447d3
Girma Ebssa <girma.ebssa@gmail.com>
2020-11-15 15:52:06 -0800

    add uuid package to create unique id

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,10 +1,10 @@
-// const data = require("../db/db.json");
+// requiring depedencies
 const fs = require("fs");
-const { join } = require("path");
+const { v4: uuid } = require('uuid');
 
+// exporting all the needed routes
 module.exports = function(app) {
-    app.get("/api/notes", (req, res) => {
-        // res.json(data)
+    app.get("/api/notes", (req, res) => {       
         // fs.readFile('./db/db.json', "utf8", (err, notes) => {
         //     if (err) throw err;            
         //     notes = JSON.parse(notes)
@@ -17,30 +17,23 @@ module.exports = function(app) {
     })
 
     app.post("/api/notes", (req, res) => {
-        let newnote = req.body;
-        //taken from https://gist.github.com/gordonbrander/2230317 
-        var ID = function () {
-            // Math.random should be unique because of its seeding algorithm.
-            // Convert it to base 36 (numbers + letters), and grab the first 9 characters
-            // after the decimal.
-            return '_' + Math.random().toString(36).substr(2, 9);
-        }    
-        let saveNote = {
-            id: ID(),
-            title: newnote.title,
-            text: newnote.text
+                    
+        let newNote = {
+            id: uuid(),
+            title: req.body.title,
+            text: req.body.text
         }
 
         fs.readFile('./db/db.json', function (err, data) {
             if(err) throw err;
             var savedNote = JSON.parse(data);
-            savedNote.push(saveNote);    
+            savedNote.push(newNote);    
             fs.writeFile('./db/db.json', JSON.stringify(savedNote), function(err){
               if (err) throw err;
               console.log('new note effectively appeneded to an array in the db.json file');
-            });
+            });            
         })
-        res.json(saveNote)      
+        res.send(newNote)              
     })
 
     app.delete("/api/notes/:id", (req, res) => {
@@ -54,7 +47,8 @@ module.exports = function(app) {
                 if (err) throw err;
                 console.log('not deleted by matching id');
             });
+            
         })
-        res.send(savedNote)
+        res.send(savedNote)        
     })
 }
